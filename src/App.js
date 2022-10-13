49af636f9799a5a0cdb87e73647417b9f3558bdf
Andryukov Alexander <alexnamecode@gmail.com>
2022-10-13 14:26:38 +0300

    posts finished

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import React  from 'react'
-import ClassCounter from "./components/ClassCounter";
+import React, {useState} from 'react'
+import './styles/App.css'
+import PostList from "./components/PostList";
 
 function App() {
+    const [postsJS, setPostsJS] = useState([
+        {id: 1, title: 'Javascript', body: 'Description'},
+        {id: 2, title: 'Javascript2', body: 'Description'},
+        {id: 3, title: 'Javascript3', body: 'Description'}
+    ])
+
+    const [postsPython, setPostsPython] = useState([
+        {id: 1, title: 'Python1', body: 'Description'},
+        {id: 2, title: 'Python2', body: 'Description'},
+        {id: 3, title: 'Python3', body: 'Description'}
+    ])
     return (
         <div className="App">
-            <ClassCounter/>
+            <PostList posts={postsJS} title="Javascript posts"/>
+            <PostList posts={postsPython} title="Python posts"/>
         </div>
     );
 }
