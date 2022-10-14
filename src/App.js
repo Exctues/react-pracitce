7e8aded9277ec4e851eee594e8169388c601bc9d
Andryukov Alexander <alexnamecode@gmail.com>
2022-10-14 07:20:40 +0300

    add ability to add new posts

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import React, {useState} from 'react'
+import React, {useRef, useState} from 'react'
 import './styles/App.css'
 import PostList from "./components/PostList";
+import MyButton from "./components/UI/button/MyButton";
+import MyInput from "./components/UI/input/MyInput";
 
 function App() {
-    const [postsJS, setPostsJS] = useState([
+    const [posts, setPosts] = useState([
         {id: 1, title: 'Javascript', body: 'Description'},
         {id: 2, title: 'Javascript2', body: 'Description'},
         {id: 3, title: 'Javascript3', body: 'Description'}
     ])
+    const [title, setTitle] = useState("")
+    const [body, setBody] = useState("")
 
-    const [postsPython, setPostsPython] = useState([
-        {id: 1, title: 'Python1', body: 'Description'},
-        {id: 2, title: 'Python2', body: 'Description'},
-        {id: 3, title: 'Python3', body: 'Description'}
-    ])
+
+    const addNewPost = (e) => {
+        e.preventDefault();
+        const newPost = {
+            id: Date.now(),
+            title,
+            body
+        }
+        setPosts([...posts, newPost])
+        setTitle('')
+        setBody('')
+    }
+
     return (
         <div className="App">
-            <PostList posts={postsJS} title="Javascript posts"/>
-            <PostList posts={postsPython} title="Python posts"/>
+            <from>
+                <MyInput
+                    value={title}
+                    onChange={e => setTitle(e.target.value)}
+                    type="text"
+                    placeholder="Название поста"
+                />
+                <MyInput
+                    value={body}
+                    onChange={e => setBody(e.target.value)}
+                    type="text"
+                    placeholder="Описание поста"
+                />
+                <MyButton onClick={addNewPost}>Создать пост</MyButton>
+            </from>
+            <PostList posts={posts} title="Javascript posts"/>
         </div>
     );
 }
